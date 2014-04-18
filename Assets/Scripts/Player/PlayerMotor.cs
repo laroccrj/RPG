bf511ecd9cf92f173c6f709dda244d8e6eec41df
Robert La Rocca <laroccrj@alfredstate.edu>
2014-04-18 10:36:56 -0400

    Basic player motor created

---
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : MonoBehaviour {
+
+	public float speed;
+
+	void Update () {
+
+		if(Input.GetKey(KeyCode.UpArrow)) {
+			this.moveUp();
+		}
+
+		if(Input.GetKey(KeyCode.DownArrow)) {
+			this.moveDown();
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			this.moveLeft();
+		}
+		
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			this.moveRight();
+		}
+
+	}
+
+	void moveUp() {
+		movement(Vector3.forward);
+	}
+
+	void moveDown() {
+		movement(Vector3.back);
+	}
+
+	void moveLeft() {
+		movement(Vector3.left);
+	}
+
+	void moveRight() {
+		movement(Vector3.right);
+	}
+
+	void movement(Vector3 direction) {
+		transform.Translate(direction * speed * Time.deltaTime);
+	}
+
+}
