f5ca18d75dfe3768d22ef365a34d7a9d27ef2f53
Robert La Rocca <laroccrj@alfredstate.edu>
2014-04-18 11:06:59 -0400

    Added basic spell casting

---
+++ Assets/Scripts/Player/Spells/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float distance;
+	public float speed;
+
+	private Vector3 startPosition;
+	private float difference;
+	
+	void Start () {
+		startPosition = transform.position;
+		Vector3 endPosition = transform.position + (transform.TransformDirection(Vector3.forward) * distance);
+		difference = Vector3.Distance(startPosition, endPosition);
+	}
+
+	void Update () {
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		transform.Translate(forward * this.speed * Time.deltaTime);
+
+		if(Vector3.Distance(this.startPosition, transform.position) > this.difference) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/Spells/Spell.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spell : MonoBehaviour {
+
+	public Projectile projectile;
+
+	public float distance;
+
+	void Update () {
+
+		//For testing
+		if(Input.GetKeyDown(KeyCode.Space)) Cast();
+
+	}
+
+	void Cast() {
+		Projectile projectile = (Projectile)GameObject.Instantiate(this.projectile, transform.position, transform.rotation);
+		projectile.distance = this.distance;
+	}
+}
